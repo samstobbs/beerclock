305e1c657266b31017c4f460bfdc9da12a9fea3c
Sam Stobbelaar <samstobbs@gmail.com>
2022-08-02 22:49:43 -0600

    Beer cans by country!

--- beerclock/src/App.vue
+++ beerclock/src/App.vue
@@ -2,11 +2,28 @@
   <Home/>
 </template>
 
-<script setup lang="ts">
-/* eslint-disable */
-// import App from './App.vue'
+<script setup>
 // import Vue, { createApp } from 'vue';
+
 import Home from './components/Home.vue';
+
+// import { createApp } from 'vue';
+// import Vue3Autocounter from 'vue3-autocounter';
+// import App from './App.vue';
+
+
+// createApp(App)
+// .component('home', Home)
+// .component('vue3-autocounter', Vue3Autocounter)
+// .mount('#app');
+
+// export default {
+//     name: 'Home-page',//Declare the components object 
+//     components: {
+//         //Declare your components here
+//         Home //This is the Child component
+//     }
+// }
 
 </script>
 
